c5dd9b1b292af4be8758a29683236ede1203b376
leehj24 <hyunji4579@gmail.com>
2023-02-14 14:08:21 +0900

    canvas

--- canvas.py
+++ canvas.py
@@ -11,7 +11,7 @@ line=canvas.create_line(10, 10, 20, 20, 20, 130, 30, 140, fill="red")
 polygon=canvas.create_polygon(50, 50, 170, 170, 100, 170, outline="yellow")
 oval=canvas.create_oval(100, 200, 150, 250, fill="blue", width=3)
 arc=canvas.create_arc(100, 100, 300, 300, start=0, extent=150, fill='red')
-
+#create_line 선, create_polygon 다각형 create_oval원 create_arc호
 canvas.pack()
 
 window.mainloop()
