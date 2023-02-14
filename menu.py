2844a11ed719d84446166557061c06b46a19ea69
leehj24 <hyunji4579@gmail.com>
2023-02-14 14:22:09 +0900

    menu

--- menu.py
+++ menu.py
@@ -14,11 +14,11 @@ menubar=tkinter.Menu(window)
 menu_1=tkinter.Menu(menubar, tearoff=0)
 menu_1.add_command(label="하위 메뉴 1-1")
 menu_1.add_command(label="하위 메뉴 1-2")
-menu_1.add_separator()
+menu_1.add_separator() #구분선 생성
 menu_1.add_command(label="하위 메뉴 1-3", command=close)
 menubar.add_cascade(label="상위 메뉴 1", menu=menu_1)
 #tear0ff-하위메뉴의 분리 기능 사용 유/무
-
+#add_cascade 상위와 하위메뉴 연결 
 menu_2=tkinter.Menu(menubar, tearoff=0, selectcolor="red")
 menu_2.add_radiobutton(label="하위 메뉴 2-1", state="disable")
 menu_2.add_radiobutton(label="하위 메뉴 2-2")
