f13f71087ce799f9e6d7fe2a5c33353e75255f3b
leehj24 <hyunji4579@gmail.com>
2023-02-21 17:28:52 +0900

    qt_log2

--- study/qt_lo2.py
+++ study/qt_lo2.py
@@ -43,23 +43,45 @@ class MyApp(QWidget):
         
         bev = BirdEyeView()
         bev.setObject(trackList)
-        grid.addWidget(bev, 0, 2) 
+        grid.addWidget(bev, 0, 1) 
+        grid.addWidget(self.firstGroup(), 0, 0)
+        
         grid.addWidget(QLabel('x값'), 1, 0)
-        grid.addWidget(QLineEdit(), 1, 1)
-        grid.addWidget(QLabel('y값'), 2, 0)
-        grid.addWidget(QLineEdit(), 2, 1) 
-        grid.addWidget(QLineEdit(), 2, 2)
-        grid.addWidget(QLineEdit(), 2, 3)
+        grid.addWidget(QLineEdit(), 2, 0)
+        grid.addWidget(QLabel('y값'), 3, 0)
+        grid.addWidget(QLineEdit(), 4, 0) 
+        grid.addWidget(QLineEdit(), 4, 1)
+        grid.addWidget(QLineEdit(), 5, 2)
               
         self.setLayout(grid)
         bev = BirdEyeView()
         # bev.move(400, 60)
         # bev.resize(50, 50)
         
-
         self.setWindowTitle('Absolute Positioning')
         self.setGeometry(300, 300, 600, 600)
         self.show()
+        
+    def firstGroup(self):
+        groupbox = QGroupBox('데이터')
+        
+        label1 = QLabel('x값', self)
+        label2 = QLabel('y값', self)
+        txt = QLineEdit(self)
+        txt.resize(60, 25)
+        txt2 = QLineEdit(self)
+        txt2.resize(60, 25)
+        
+        vbox = QVBoxLayout()
+        vbox.addWidget(label1)
+        vbox.addWidget(label2)
+        vbox.addWidget(txt)
+        vbox.addWidget(txt2)
+        groupbox.setLayout(vbox)
+
+        return groupbox
+    
+       
         
         
 if __name__ == '__main__':
