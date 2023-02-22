64353c530964d2b11604ab1213eb4d97efc133d1
leehj24 <hyunji4579@gmail.com>
2023-02-22 16:52:52 +0900

    qt_timter

--- study/qt_lo2.py
+++ study/qt_lo2.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+import pandas as pd
 
 class BirdEyeView(QWidget):
     
@@ -24,12 +25,22 @@ class BirdEyeView(QWidget):
         
     def setObject(self, trackList):  
         self.trackList = trackList
-
+        
+        self.timer = QTimer(self)
+        self.timer.start(30)
+        self.timer.timeout.connect(self.timeout_run)
+        
+    # def timeout_run(self):
+    #     i=0
+    #     for i in self.trackList[i]:
+    #         i =  i+1
+    #     self.update()
+               
 class Track:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
+        
 class MyApp(QWidget):
 
     def __init__(self):
@@ -39,27 +50,25 @@ class MyApp(QWidget):
     def initUI(self):
         grid = QGridLayout()
         
-        trackList = [Track(10, 10), Track(50, 50)]
+        trackList = [Track(10, 10), Track(50, 50), Track(80,80)]
         
         bev = BirdEyeView()
         bev.setObject(trackList)
         grid.addWidget(bev, 0, 1) 
-        grid.addWidget(self.firstGroup(), 0, 0)
         
-        grid.addWidget(QLabel('x값'), 1, 0)
-        grid.addWidget(QLineEdit(), 2, 0)
-        grid.addWidget(QLabel('y값'), 3, 0)
-        grid.addWidget(QLineEdit(), 4, 0) 
-        grid.addWidget(QLineEdit(), 4, 1)
-        grid.addWidget(QLineEdit(), 5, 2)
+        self.lb = QLabel(self)
+        
+        grid.addWidget(self.firstGroup(), 0, 0)
+        grid.addWidget(self.secondGroup(), 1, 0)
+        grid.addWidget(self.thirdGroup(), 2, 0)
+        grid.addWidget(self.lb, 3, 0)
+        grid.addWidget(QTextBrowser(), 2, 1) #QScrollArea
               
         self.setLayout(grid)
-        bev = BirdEyeView()
-        # bev.move(400, 60)
-        # bev.resize(50, 50)
+        grid = QGridLayout()
         
         self.setWindowTitle('Absolute Positioning')
-        self.setGeometry(300, 300, 600, 600)
+        self.setGeometry(300, 300, 700, 600)
         self.show()
         
     def firstGroup(self):
@@ -68,21 +77,83 @@ class MyApp(QWidget):
         label1 = QLabel('x값', self)
         label2 = QLabel('y값', self)
         txt = QLineEdit(self)
-        txt.resize(60, 25)
+        
         txt2 = QLineEdit(self)
         txt2.resize(60, 25)
         
         vbox = QVBoxLayout()
         vbox.addWidget(label1)
-        vbox.addWidget(label2)
         vbox.addWidget(txt)
+        txt.resize(60, 25)
+        vbox.addWidget(label2)
         vbox.addWidget(txt2)
         groupbox.setLayout(vbox)
 
         return groupbox
     
-       
+    def secondGroup(self):
+        groupbox = QGroupBox('데이터')
         
+        btn1 = QPushButton('start', self)
+        btn1.clicked.connect(self.buttonClicked)
+        
+        btn2 = QPushButton('dialog', self)
+        btn2.clicked.connect(self.dialog)
+        
+        btn3 = QPushButton('stop', self)
+        btn3.clicked.connect(self.bt_stop)
+        
+        btn4 = QPushButton('play', self)
+        btn4.clicked.connect(self.bt_play)
+        
+        vbox = QVBoxLayout()
+        vbox.addWidget(btn1)
+        vbox.addWidget(btn2)
+        vbox.addWidget(btn3)
+        vbox.addWidget(btn4)
+        groupbox.setLayout(vbox)
+        
+        return groupbox
+    
+    #https://hongsusoo.github.io/angleprice_1/
+    def thirdGroup(self):
+        groupbox = QGroupBox('테이블')
+        self.tableWidget = QTableWidget()
+        self.tableWidget.setRowCount(2)
+        self.tableWidget.setColumnCount(2)
+        self.tableWidget.setItem(0,0,QTableWidgetItem('Apple'))
+        
+        layout = QVBoxLayout()
+        layout.addWidget(self.tableWidget)
+        groupbox.setLayout(layout)
+        return groupbox
+    
+    def bt_play(self):
+        self.timer = QTimer(self)
+        self.timer.start(50)
+        self.timer.timeout.connect(self.timeout_run)
+    
+    def timeout_run(self):
+        i=0
+        for i in self.trackList:
+            i =  i+1
+            if i < 100:
+                continue
+        self.update()
+    
+    def bt_stop(self):
+        self.timer.stop()
+    
+    def buttonClicked(self):
+        self.lb.setText('self.x,y')
+        
+    def dialog(self):
+        d= QDialog
+        d.setWindowTitle('Dialog')
+        d.setWindowModality(Qt.ApplicationModal)
+        d.setGeometry(300, 300, 700, 700)
+        d.show()
+    
         
 if __name__ == '__main__':
     app = QApplication(sys.argv)
---
+++ study/timer.py
@@ -0,0 +1,65 @@
+import sys
+
+from PyQt5.QtCore import pyqtSlot, QTimer, Qt, QCoreApplication
+from PyQt5.QtGui import QPainter, QPen
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.pos_x = 0
+        self.direction = 1
+        self.speed = 3
+
+    def initUI(self):
+        # 윈도우 설정
+        self.setGeometry(300, 300, 300, 300)  # x, y, w, h
+        self.setWindowTitle('QPaint Move')
+
+        # Timer 설정
+        
+
+        # 창닫기 버튼
+        btn = QPushButton('stop', self)
+        btn.move(1,1)
+        btn.resize(btn.sizeHint())
+        btn.clicked.connect(self.stop)
+        
+        btn2 = QPushButton('play', self)
+        btn2.move(100,1)
+        btn2.resize(btn.sizeHint())
+        btn2.clicked.connect(self.play)
+
+    def paintEvent(self, e):
+        qp = QPainter()
+        qp.begin(self)
+        self.draw_point(qp)
+        qp.end()
+
+    def draw_point(self, qp):
+        qp.setPen(QPen(Qt.blue, 8))
+        qp.drawPoint(self.pos_x, 100)
+
+    def timeout_run(self):
+        if self.pos_x < 0 or self.pos_x > self.width() - 8:
+            self.direction *= -1
+        self.pos_x = self.pos_x + (self.direction * self.speed)
+        print(self.pos_x, self.width())
+        self.update()
+        
+    def stop(self):
+        self.timer.stop()
+        
+    def play(self):
+        self.timer = QTimer(self)
+        self.timer.start(50)
+        self.timer.timeout.connect(self.timeout_run)
+        
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = MainWindow()
+    mainWindow.show()
+    sys.exit(app.exec_())
