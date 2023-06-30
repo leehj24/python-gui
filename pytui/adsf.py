d63b8c7dd48f972ec87218097d27734725549bd8
hyeonji.lee <hyeonji.lee@hlcompany.com>
2023-06-30 14:22:16 +0900

    delete

--- pytui/adsf.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-file1 =  pd.read_excel('./scenario_1.xlsx', sheet_name = 0)
-
-print(file1.columns)
--- study/pandas.py
+++
@@ -1,9 +0,0 @@
-# import matplotlib.pyplot as plt
-# from pandas import *
-# import pandas as pd
-# import numpy as np
-
-# x= np.arange(-50,50,1)
-# y = 1*x**3 + 0*x**2 + 1*x +1
-# plt.plot(x,y)
-# plt.show()
